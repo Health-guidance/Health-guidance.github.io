4857a3a237653543ad29b7aeb1ce98b27b67cefc
pablo-codes <112553944+pablo-codes@users.noreply.github.com>
2023-11-22 18:40:21 +0100

    feat: Designing the chatbot

--- src/components/Chatbot/Chatbot.js
+++ src/components/Chatbot/Chatbot.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Chatbot = () => {
     return (
-        <div className="container bg-white d-flex flex-column">
-            <div className="card mb-3">
+        <div className="container bg-white d-flex flex-column align-items-center">
+            <div className="card mb-3 w-50">
                 <div className="card-header text-dark text-base d-flex flex-column align-items-stretch mt-2.5 max-md-max-w-full">
-                    <h5 className="card-title">AI H-link</h5>
+                    <h5 className="card-title text-center">AI H-link</h5>
                 </div>
             </div>
             <div className="text-dark text-center text-base font-semibold leading-7 tracking-wider text-center max-w-326px mt-20 max-md-mt-10">
@@ -16,34 +16,36 @@ const Chatbot = () => {
                 experiencing any pain? You can type your symptoms or select from the
                 list below
             </div>
-            <div className="card mt-14">
+            <div className="card mt-4 rounded-pill w-75">
                 <div className="card-body d-flex flex-column align-items-center justify-content-center max-w-360px">
                     <div className="text-dark text-base leading-5 max-w-271px">I feel feverish and cold</div>
                     <div className="text-muted text-base leading-5 max-w-302px mt-3">What could be the cause?</div>
                 </div>
             </div>
-            <div className="card mt-4">
+            <div className="card mt-4 rounded-pill w-75">
                 <div className="card-body d-flex flex-column align-items-center justify-content-center max-w-360px">
                     <div className="text-dark text-base leading-5 max-w-271px">My head aches</div>
                     <div className="text-muted text-base leading-5 max-w-302px mt-2">How can I treat headaches?</div>
                 </div>
             </div>
-            <div className="card mt-4">
+            <div className="card mt-4 rounded-pill w-75">
                 <div className="card-body d-flex flex-column align-items-center justify-content-center max-w-360px">
                     <div className="text-dark text-base leading-5 max-w-271px">How can I stop abdominal pains?</div>
                     <div className="text-muted text-base leading-5 max-w-302px mt-2">When should I see a doctor?</div>
                 </div>
             </div>
-            <div className="card mt-4">
+            <div className="card mt-4 rounded-pill mb-5 w-75">
                 <div className="card-body d-flex flex-column align-items-center justify-content-center max-w-360px">
                     <div className="text-dark text-base leading-5 max-w-271px">How can I treat hot water burns?</div>
                     <div className="text-muted text-base leading-5 max-w-302px mt-3">Home remedies for hot water burns</div>
                 </div>
             </div>
-            <div className="card d-flex justify-content-between align-items-center mt-36 px-3 py-4 rounded-2xl">
-                <button className="btn btn-primary grow shrink basis-auto">Send a message</button>
-                <img src="https://cdn.builder.io/api/v1/image/assets/TEMP/2e7b137f-dba9-46c0-a621-65a2d700ae37?apiKey=9a437da56f594c71b21a541f7ee14f1e" className="img-fluid" alt="" />
-            </div>
+            <form className="form-floating mt-10 w-75 rounded-pill">
+
+
+                <input type="email" class="form-control rounded-pill" id="floatingInput" placeholder="Input message here" />
+
+            </form>
             <div className="text-dark text-center text-sm leading-4 underline text-center mt-3">
                 <a href="https://help.openai.com/en/articles/6825453-chatgpt-release-notes" target="_blank">
                     ChatGPT August 3 Version
